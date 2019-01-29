d664e390d762b7fcfb038ddbfacb8c45d3dc0111
aranjbar <ali1377ranjbar@gmail.com>
2019-01-28 17:50:28 -1200

    element-wise multiply

--- dens/dSN.m
+++ dens/dSN.m
@@ -4,6 +4,6 @@ function dens = dSN (y, varargin)
     varargin = validate_input(varargin, [0, 1, 1]);
     [mu, sigma2, shape] = varargin{:};
 
-    dens = 2 * normpdf(y, mu, sqrt(sigma2)) ...
-             * normcdf(shape*(y - mu)/sqrt(sigma2));
+    dens = 2 .* normpdf(y, mu, sqrt(sigma2)) ...
+             .* normcdf(shape .* (y - mu)/sqrt(sigma2));
 end
