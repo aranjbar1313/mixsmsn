fe6dec353663bdb30f85b8c6f2878ccfde174a09
aranjbar <ali1377ranjbar@gmail.com>
2019-01-28 19:31:20 -1200

    density of SSI mixtures

---
+++ dens/d_mixedSS.m
@@ -0,0 +1,11 @@
+%% Density of SSI Mixtures %%
+function dens = d_mixedSS (x, pi1, mu, sigma2, shape, nu)
+
+    % x: the data vector
+    % other parameters must be of the row-vector type of dimension g (qtd of mixtures)
+    g = numel(pi1);
+    dens = 0;
+    for j = 1 : g 
+        dens = dens + pi1(j) .* dSS(x, mu(j), sigma2(j), shape(j), nu);
+    end
+end
