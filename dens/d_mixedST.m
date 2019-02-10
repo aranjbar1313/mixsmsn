a6262afc25fbe5328a91314eeb1a82df05b225e8
Ali <ali1377ranjbar@gmail.com>
2019-02-10 18:59:12 +0330

    parallelized for loop

--- dens/d_mixedST.m
+++ dens/d_mixedST.m
@@ -5,7 +5,7 @@ function dens = d_mixedST (x, pi1, mu, sigma2, shape, nu)
     % other parameters must be of the row-vector type of dimension g (qtd of mixtures)
     g = numel(pi1);
     dens = 0;
-    for j = 1 : g 
+    parfor j = 1 : g 
         dens = dens + pi1(j) .* dt_ls(x, mu(j), sigma2(j), shape(j), nu);
     end
 end
